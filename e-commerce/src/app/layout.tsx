8ddc685eda0de1025166bbdeac26a0b039e6b5cd
Aydoğan Uyanıkoğlu <aydogan.uyanikoglu@erka.work>
2024-08-16 17:32:53 +0300

    new

--- e-commerce/src/app/layout.tsx
+++ e-commerce/src/app/layout.tsx
@@ -2,11 +2,20 @@ import { Montserrat } from 'next/font/google';
 import "./globals.css";
 import { MantineProvider } from '@mantine/core';
 import "@mantine/core/styles.css";
+import { Metadata } from 'next';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '700'],
 });
+
+export const metadata: Metadata = {
+  title: {
+    template: "ecommerce",
+    default: "ecommerce",
+  },
+  description: "ecommerce app by atmaca",
+};
 
 export default function RootLayout({
   children,
